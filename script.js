9788427c4bae74afa4e3dfc5caa5d24724730e68
Himel Islam <himel7100@gmail.com>
2024-03-11 02:26:39 +0600

    navbar added

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-var tl = gsap.timeline();
+function loading() {
+    var tl = gsap.timeline();
 
-tl.to("#yellow1", {
-    top: "-100%",
-    delay: 0.5,
-    duration: 0.7,
-    ease: "expo-out"
-})
-tl.from("#yellow2", {
-    top: "100%",
-    delay: 0.6,
-    duration: 0.7,
-    ease: "expo-out"
-}, "anim")
-tl.to("#loader h1", {
-    delay: 0.6,
-    duration: 0.7,
-    color: "black"
-}, "anim")
-tl.to("#loader", {
-    opacity: 0
+    tl.to("#yellow1", {
+        top: "-100%",
+        delay: 0.5,
+        duration: 0.7,
+        ease: "expo-out"
+    })
+    tl.from("#yellow2", {
+        top: "100%",
+        delay: 0.6,
+        duration: 0.7,
+        ease: "expo-out"
+    }, "anim")
+    tl.to("#loader h1", {
+        delay: 0.6,
+        duration: 0.7,
+        color: "black"
+    }, "anim")
+    tl.to("#loader", {
+        opacity: 0
+    })
+    tl.to("#loader", {
+        display: "none"
+    })
+}
+
+loading();
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    scroll: true
 })
